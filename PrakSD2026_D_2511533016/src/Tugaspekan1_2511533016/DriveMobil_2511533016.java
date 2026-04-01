dcb563d2d140dd0029c05e51fe7e490fe006c9bd
axcer <axcer@FedoArgadyaksa>
2026-04-01 21:17:23 +0700

    Tugas Pekan 1

---
+++ PrakSD2026_D_2511533016/src/Tugaspekan1_2511533016/DriveMobil_2511533016.java
@@ -0,0 +1,38 @@
+package Tugaspekan1_2511533016;
+
+import java.util.ArrayList;
+
+public class DriveMobil_2511533016 {
+
+	public static void main(String[] args) {
+		ArrayList<Mobil_2511533016> daftarMobil = new ArrayList<>();
+			
+			// Tambah mobil
+		Mobil_2511533016 m1 = new Mobil_2511533016("Avanza", 2020, 1300, 150000000, "Toyota");
+		Mobil_2511533016 m2 = new Mobil_2511533016("Brio", 2022, 1200, 170000000, "Honda");
+			
+		daftarMobil.add(m1);
+		daftarMobil.add(m2);
+			
+		System.out.println("=== Data Mobil ===");
+		for (Mobil_2511533016 m : daftarMobil) {
+			m.tampil();
+		}
+			
+		// Hapus mobil (contoh hapus index ke-0)
+		daftarMobil.remove(0);
+			
+		System.out.println("=== Setelah Hapus ===");
+		for (Mobil_2511533016 m : daftarMobil) {
+			m.tampil();
+		}
+			
+		// Contoh penggunaan setter (mutator)
+		m2.setHarga(180000000);
+			
+		System.out.println("=== Setelah Upadate Harga ===");
+		m2.tampil();
+					
+		}
+
+	}
